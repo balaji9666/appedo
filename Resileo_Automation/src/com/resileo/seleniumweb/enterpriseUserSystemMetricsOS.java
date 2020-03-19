a5a27524a681aa9b09047e5f4b21d8227324520f
balaji9666 <balaji1996@gmail.com>
2020-03-20 00:48:10 +0530

    Balaji

--- Resileo_Automation/src/com/resileo/seleniumweb/enterpriseUserSystemMetricsOS.java
+++ Resileo_Automation/src/com/resileo/seleniumweb/enterpriseUserSystemMetricsOS.java
@@ -127,7 +127,7 @@ public class enterpriseUserSystemMetricsOS extends ReusableMethods {
 			browser.findElement(getObject(appedo_enterprise_dashboardname)).click();
 			browser.findElement(getObject(appedo_enterprise_dashboardname)).sendKeys(dashboardname);
 			Thread.sleep(2000);
-			browser.findElement(getObject(appedo_btnSave)).click();
+			//browser.findElement(getObject(appedo_btnSave)).click();
 			
 			// Click on save
 			browser.findElement(getObject(appedo_btnSave)).click();
