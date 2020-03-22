de0ac1b74fb9cd4c739d65786e8bb5fce97e9285
balaji9666 <balaji1996@gmail.com>
2020-03-23 01:45:05 +0530

    Balaji

--- Resileo_Automation/src/com/resileo/seleniumweb/visualizerAddQuery.java
+++ Resileo_Automation/src/com/resileo/seleniumweb/visualizerAddQuery.java
@@ -3,7 +3,6 @@ package com.resileo.seleniumweb;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
-import org.testng.Assert;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import com.relevantcodes.extentreports.LogStatus;
